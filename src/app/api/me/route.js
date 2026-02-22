5760b0038ec3ae0484bb2adf9b7846c004576718
Charcrede <charcrede@gmail.com>
2026-02-22 02:59:43 +0100

    log token

--- src/app/api/me/route.js
+++ src/app/api/me/route.js
@@ -4,8 +4,8 @@ import { cookies } from 'next/headers';
 export async function GET() {
   const token = cookies().get('auth_token')?.value;
 
+  console.log(token)
   if (!token) return Response.json({ error: 'Not authenticated' }, { status: 401 });
-
   const user = await fetch(`${process.env.NEXT_PUBLIC_API}users/me`, {
     headers: { Authorization: `Bearer ${JSON.parse(token?.slice(2)).access_token}` }
   }).then(r => r.json());
